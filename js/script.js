4429bf7fce93989202dcd110e90f09ca4cf2830f
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-11 20:28:08 -0300

    90% del swiper - quedo hermoso-

--- js/script.js
+++ js/script.js
@@ -61,6 +61,34 @@ document.querySelector('.home').onmouseleave = () =>{
 
 };
 
+// js del swiper 
+
+  var swiper = new Swiper(".vehiculos-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
 
 
 
