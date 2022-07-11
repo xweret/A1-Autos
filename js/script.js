905f423d209798b203fd57ce6bbb59ac1c570657
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-11 12:44:53 -0300

    Terminado el nav y casi la home

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+// mostrar y ocultar el form con el boton del nav
+
+document.querySelector('#login-btn').onclick = () =>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+  }
+
+  document.querySelector('#close-login-form').onclick = () =>{
+    document.querySelector('.login-form-container').classList.remove('active');
+  }
+  
 // funcion para mostrar, en deskstop, una sombra cuando el usuario escrolea
 window.onscroll = () => {
 
@@ -17,3 +28,13 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+window.onload = () => {
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
